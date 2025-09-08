57b453932299ac8028c170bb0dff655f6d74df72
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-08 13:25:50 -0400

    week 2

--- web/week2/app.js
+++ web/week2/app.js
@@ -1,1 +1,37 @@
-console.log('Hello World!');
+let experiment = 'Stroop';
+console.log(typeof experiment); // String
+console.log(experiment.charAt(0)); // 'S'
+
+let welcomeMessage = `
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+`;
+// console.log(welcomeMessage)
+// console.log(typeof experiment); // string
+// console.log(trialCountMax.charAt1); // t
+
+
+let trialCountMax = 20.5;
+// console.log(typeof trialCountMax); // number
+// console.log(experiment.charAt(0));
+
+// TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+// alert('Welcome to the ' + experiment + ' experiment!')
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfwayCount = trialCountMax / 2;
+
+// console.log(halfwayCount); // Expected: 20
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 > 15); // false
