41fb156db8f87b6082f8631a596d8e53beafd9d7
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-09 16:39:55 -0400

    MRT part A

---
+++ web/week2/mrt.js
@@ -0,0 +1,8 @@
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.");
+let answer1 = prompt("What is 4 + 5?");
+alert("You answered " + answer1);
+let answer2 = prompt("What is 6 + 7?");
+alert("You answered " + answer2);
+let answer3 = prompt("What is 2 + 9?");
+alert("You answered " + answer3);
+alert("Thank you for your participation!");
