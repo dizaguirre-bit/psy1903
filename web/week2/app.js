11db41efc9e94ed2c2e476c59b2c18022784bfd4
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-09 16:41:29 -0400

    misc changes

--- web/week2/app.js
+++ web/week2/app.js
@@ -1,37 +1,54 @@
-let experiment = 'Stroop';
-console.log(typeof experiment); // String
-console.log(experiment.charAt(0)); // 'S'
+// let experiment = 'Stroop';
+// console.log(typeof experiment); // String
+// console.log(experiment.charAt(0)); // 'S'
 
-let welcomeMessage = `
-Welcome to our ` + experiment + ` experiment! 
-Please read the instructions carefully.
-Welcome to our ` + experiment + ` experiment! 
-Please read the instructions carefully.
-Welcome to our ` + experiment + ` experiment! 
-Please read the instructions carefully.
-`;
-// console.log(welcomeMessage)
-// console.log(typeof experiment); // string
-// console.log(trialCountMax.charAt1); // t
+// let welcomeMessage = `
+// Welcome to our ` + experiment + ` experiment! 
+// Please read the instructions carefully.
+// Welcome to our ` + experiment + ` experiment! 
+// Please read the instructions carefully.
+// Welcome to our ` + experiment + ` experiment! 
+// Please read the instructions carefully.
+// `;
+// // console.log(welcomeMessage)
+// // console.log(typeof experiment); // string
+// // console.log(trialCountMax.charAt1); // t
 
 
-let trialCountMax = 20.5;
-// console.log(typeof trialCountMax); // number
-// console.log(experiment.charAt(0));
+// let trialCountMax = 20.5;
+// // console.log(typeof trialCountMax); // number
+// // console.log(experiment.charAt(0));
 
-// TODO: Randomize colors
-let colors = ['red', 'green', 'blue'];
+// // TODO: Randomize colors
+// let colors = ['red', 'green', 'blue'];
 
 // alert('Welcome to the ' + experiment + ' experiment!')
 
-trialCountMax = 40;
+// trialCountMax = 40;
 
-// At the halfway point we will display a pause screen
-let halfwayCount = trialCountMax / 2;
+// // At the halfway point we will display a pause screen
+// let halfwayCount = trialCountMax / 2;
 
-// console.log(halfwayCount); // Expected: 20
+// // console.log(halfwayCount); // Expected: 20
 
-let correct = false;
-console.log(typeof correct);
+// let correct = false;
+// console.log(typeof correct);
 
-console.log(10 > 15); // false
+// console.log(10 <= 15); // false
+
+// let answer = prompt('What is 1 + 1?');
+// console.log(answer);
+
+// answer = prompt('What is 9 + 1');
+// console.log(answer);
+
+console.log("Hello World");
+
+let courseName = 'Programming for Psychologists';
+console.log(courseName.length); // Output: 29
+
+console.log(courseName.toUpperCase());
+
+console.log(courseName.indexOf("Psychologists"));
+
+console.log(courseName.replace("Programming", "Coding"));
