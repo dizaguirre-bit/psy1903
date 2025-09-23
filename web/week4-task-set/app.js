9fb420b4b979dc3d2e166fcdbf6468b1a7120560
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-23 16:50:03 -0400

    Week 4 Task Set Q4

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -5,17 +5,29 @@
 // console.log(celsiusToFahrenheit(10)); // Expected output: 50
 // console.log(celsiusToFahrenheit(-5)); // Expected output: 23
 
-function convertTemp(temp, convertTo) {
-    if (convertTo == 'c') {
-        let convertTemp = (temp * 1.8) + 32;
-        return convertTemp
-    }
+// function convertTemp(temp, convertTo) {
+//     if (convertTo == 'c') {
+//         let convertTemp = (temp * 1.8) + 32;
+//         return convertTemp
+//     }
 
-    else if (convertTo == 'f') {
-        let convertTemp = (temp - 32) / 1.8;
-        return convertTemp
+//     else if (convertTo == 'f') {
+//         let convertTemp = (temp - 32) / 1.8;
+//         return convertTemp
+//     }
+// }
+
+// console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
+// console.log(convertTemp(10, 'f')); // Expected output: 50
+
+let getWordLength = [];
+function getWordLengths(words) {
+
+    for (let word of words) {
+        getWordLength.push(word.length)
     }
+    return getWordLength
 }
 
-console.log(convertTemp(10, 'c')); // Expected output: -12.222222222222221
-console.log(convertTemp(10, 'f')); // Expected output: 50
+let words = ['apple', 'banana', 'cherry', 'pear', 'grape'];
+console.log(getWordLengths(words)); // Expected output: [5, 6, 6, 4, 5]
