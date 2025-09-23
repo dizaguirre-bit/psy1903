fbf385848ab1d380a1dd3ae15f74dc83329c2434
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-23 16:09:52 -0400

    Week 4 Task Set Q2

---
+++ web/week4-task-set/app.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(celsius) {
+    let celsiusToFahrenheit = (celsius * 1.8) + 32
+    return celsiusToFahrenheit
+}
+console.log(celsiusToFahrenheit(10)); // Expected output: 50
+console.log(celsiusToFahrenheit(-5)); // Expected output: 23
