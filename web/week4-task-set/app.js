5f83ffe0d522bedd048143cdb1c2fa452417d1cc
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-23 20:10:07 -0400

    Task Set Week 4 Q8

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -5,6 +5,8 @@
 // console.log(celsiusToFahrenheit(10)); // Expected output: 50
 // console.log(celsiusToFahrenheit(-5)); // Expected output: 23
 
+// const e = require("express");
+
 // function convertTemp(temp, convertTo) {
 //     if (convertTo == 'c') {
 //         let convertTemp = (temp * 1.8) + 32;
@@ -58,25 +60,67 @@
 // console.log(getOddNumbers([1, 2, 3, 4, 5])); // Expected output: [1, 3, 5]
 // console.log(getOddNumbers([12, 45, 10, 11, 61])); // Expected output: [45, 11, 61]
 
-function filterNumbers(numbers, evenOrOdd) {
-    let results = [];
-    for (let number of numbers) {
-        if (evenOrOdd == 'even') {
-            if (number % 2 == 0) {
-                results.push(number);
-            }
+// function filterNumbers(numbers, evenOrOdd) {
+//     let results = [];
+//     for (let number of numbers) {
+//         if (evenOrOdd == 'even') {
+//             if (number % 2 == 0) {
+//                 results.push(number);
+//             }
+//         }
+//         else if (evenOrOdd == 'odd') {
+//             if (number % 2 != 0) {
+//                 results.push(number);
+//             }
+//         }
+//     }
+//     return (results)
+// }
+
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // Expected output: [2, 4]
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // Expected output: [1, 3, 5]
+
+// console.log(filterNumbers([45, 10, 11, 61], 'even')); // Expected output: [10]
+// console.log(filterNumbers([45, 10, 11, 61], 'odd')); // Expected output: [45, 11, 61]
+
+alert(`Welcome to the even/odd response time task. 
+
+You are about to see a series of numbers.
+
+If the number you see is EVEN, type the letter 'e'. 
+If the number you see is odd, type the letter 'o'. 
+
+Please answer as quickly and accurately as possible.`)
+let results = [];
+for (let i = 0; i < 5; i++) {
+    let start = Date.now();
+    let number = Math.floor(Math.random() * 20) + 1;
+    let response = prompt(`Number: ${number} 
+        Type the letter 'e' for EVEN.
+        Type the letter 'o' for ODD.`);
+    let end = Date.now();
+    let responseTime = (end - start) / 1000;
+    if (number % 2 == 0) {
+        if (response == 'e') {
+            correct = true;
         }
-        else if (evenOrOdd == 'odd') {
-            if (number % 2 != 0) {
-                results.push(number);
-            }
+        else {
+            correct = false;
         }
     }
-    return (results)
+    else if (number % 2 != 0) {
+        if (response == 'o') {
+            correct = true;
+        }
+        else {
+            correct = false;
+        }
+    }
+    results.push({
+        number: number,
+        response: response,
+        correct: correct,
+        responseTime: responseTime
+    })
 }
-
-console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // Expected output: [2, 4]
-console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // Expected output: [1, 3, 5]
-
-console.log(filterNumbers([45, 10, 11, 61], 'even')); // Expected output: [10]
-console.log(filterNumbers([45, 10, 11, 61], 'odd')); // Expected output: [45, 11, 61]
+console.log(results);
