fa7d179185c55d5398fa9d95f86de102379b59c0
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-09-30 16:34:51 -0400

    Week 5 Task Set Q4

--- web/week-5-task-set/experiment.js
+++ web/week-5-task-set/experiment.js
@@ -7,11 +7,63 @@ let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
     <h1>Welcome to the Math Response Time Task!</h1> 
-    <p>In this experiment...</p>
+    <p>In this experiment, you will be shown a series of math questions.</p>
+    <p>Please answer as quickly and accurately as possible.</p>
     <p>Press SPACE to begin the first part.</p>
     `,
     choices: [' '],
 };
+
 timeline.push(welcomeTrial);
+
+// Experimental Trial
+for (let condition of conditions) {
+    let mathTrial = {
+        type: jsPsychSurveyHtmlForm,
+        preamble: `
+            <p>What is ${condition.num1} + ${condition.num2}?</p>
+            `,
+        html: `<p><input type='text' name='answer' id='answer'></p>`,
+        autofocus: 'answer', // id of the field we want to auto-focus on when the trial starts
+        button_label: 'Submit Answer',
+        data: {
+            collect: true,
+        },
+        // Whether correct or incorrect
+        on_finish: function (data) {
+            data.num1 = condition.num1;
+            data.num2 = condition.num2;
+            data.correctAnswer = condition.correctAnswer;
+            data.answer = data.response.answer;
+            if (data.answer == condition.correctAnswer) {
+                data.correct = true;
+            }
+            else {
+                data.correct = false;
+            }
+        }
+    }
+    timeline.push(mathTrial);
+}
+
+
+// Debrief
+let debriefTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: `
+    <h1>Thank you!</h1>
+    <p>You may now close this tab</p>
+    `,
+    choices: ['NO KEYS'],
+    on_start: function () {
+        let data = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['collect', 'response', 'trial_type', 'trial_index', 'plugin_version'])
+            .csv();
+        console.log(data);
+    }
+}
+timeline.push(debriefTrial)
 
 jsPsych.run(timeline);
