239dedbd8e544afcb6aa94dc208bab77828cc439
Deano Izaguirre <dizaguirre@college.harvard.edu>
2025-10-07 11:14:22 -0400

    Week 6 Notes

--- web/lexical-decision/experiment.js
+++ web/lexical-decision/experiment.js
@@ -2,18 +2,32 @@ let jsPsych = initJsPsych();
 
 let timeline = [];
 
+colors = jsPsych.randomization.repeat(['red', 'green', 'blue'], 1);
+let color = colors.pop();
+
+let trial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['f', 'j'],
+    stimulus: `
+        <span class = '${color}'>ball</span>`
+    ,
+};
+
+// timeline.push(trial);
+
 // Welcome
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1>Welcome to the Lexical Decision Task!</h1> 
+        <h1 class = 'instructions'> Welcome to the Lexical Decision Task!</h1> 
     <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
     <p>There are three parts to this experiment.</p>
-    <p>Press SPACE to begin the first part.</p>
-    `,
+    <p class = 'instructions'>Press <span class = 'key'>SPACE</span> to begin the first part.</p>
+`,
     choices: [' '],
 };
 timeline.push(welcomeTrial);
+
 // Showed word or psuedo word (on repeat)
 
 
@@ -24,12 +38,12 @@ for (let block of conditions) {
     let blockIntroTrial = {
         type: jsPsychHtmlKeyboardResponse,
         stimulus: `
-            <h1>${block.title}</h1>
+    <h1> ${block.title}</h1 >
             <p>You are about to see a series of ${block.count} characters.</p>
             <p>If the characters make up a word, press the F key.</p>
             <p>If the characters do not make up a word, press the J key.</p>
             <p>Press SPACE to begin.</p>
-            `,
+`,
         choices: [' '],
     };
     timeline.push(blockIntroTrial);
@@ -37,7 +51,7 @@ for (let block of conditions) {
     for (let condition of blockConditions) {
         let conditionTrial = {
             type: jsPsychHtmlKeyboardResponse,
-            stimulus: `<h1>${condition.characters}</h1>`,
+            stimulus: `<h1> ${condition.characters}</h1 > `,
             choices: ['f', 'j'],
             data: {
                 collect: true,
@@ -61,22 +75,70 @@ for (let block of conditions) {
     }
 }
 
+// Results
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: `
+    <h1> Please wait...</h1 >
+     <span class='loader'></span>
+        <p>We are saving the results of your inputs.</p>
+`,
+    on_start: function () {
+        //  ⭐ Update the following three values as appropriate ⭐
+        let prefix = 'lexical-decision';
+        let dataPipeExperimentId = 'your-experiment-id-here';
+        let forceOSFSave = false;
+
+        // Filter and retrieve results as CSV data
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+            .csv();
+
+        console.log(results);
+
+        // Generate a participant ID based on the current timestamp
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        // Dynamically determine if the experiment is currently running locally or on production
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        // Send the results to our saving end point
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            // jsPsych.finishTrial();
+        })
+    }
+}
+timeline.push(resultsTrial);
+
 // Debrief
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1>Thank you!</h1>
-    <p>You may now close this tab</p>
-    `,
+    <h1> Thank you!</h1 >
+        <p>You may now close this tab</p>
+`,
     choices: ['NO KEYS'],
-    on_start: function () {
-        let data = jsPsych.data
-            .get()
-            .filter({ collect: true })
-            .ignore(['trial_type', 'trial_index', 'plugin_version', 'collect', 'stimulus'])
-            .csv();
-        console.log(data);
-    }
 }
 timeline.push(debriefTrial)
 
